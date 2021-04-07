27764fb4f4a8c9f3a5e807478d451a935f410bc2
clearlyyy <73618253+clearlyyy@users.noreply.github.com>
2021-04-07 16:40:03 -0700

    Update sfml-gui.hpp

--- sfml-gui.hpp
+++ sfml-gui.hpp
@@ -1,11 +1,13 @@
 #include <Graphics.hpp>
 #include "sfml-gui-text.hpp"
+#include "sfml-gui-checkbox.hpp"
 
 class gui
 {
 public:
 
-	gui(std::string WINDOW_TITLE, sf::Vector2f GUI_SIZE, sf::Vector2f GUI_POSITION, sf::Color GUI_BACKGROUND_COLOR, sf::Color TOP_BAR_COLOR, sf::Color TEXT_COLOR)
+	gui(std::string WINDOW_TITLE, sf::Vector2f GUI_SIZE, sf::Vector2f GUI_POSITION, sf::Color GUI_BACKGROUND_COLOR,
+		sf::Color TOP_BAR_COLOR, sf::Color TEXT_COLOR)
 	{
 		GUI_BACKGROUND.setSize(GUI_SIZE);
 		GUI_BACKGROUND.setFillColor(GUI_BACKGROUND_COLOR);
@@ -26,17 +28,26 @@ public:
 		WINDOW_TEXT.setString(WINDOW_TITLE);
 
 
+
 	}
 
+	sf::RectangleShape getGUI()
+	{
+		return GUI_BACKGROUND;
+	}
+	sf::RectangleShape getGUIColor()
+	{
+		return TOP_BAR;
+	}
 
-	void DRAW_GUI(sf::RenderWindow &window)
+	void DRAW_GUI(sf::RenderWindow& window)
 	{
 		window.draw(GUI_BACKGROUND);
 		window.draw(TOP_BAR);
 		window.draw(WINDOW_TEXT);
 	}
 
-	void UPDATE_GUI(sf::RenderWindow &window)
+	void UPDATE_GUI(sf::RenderWindow& window)
 	{
 		if (sf::Mouse::getPosition(window).x >= TOP_BAR.getPosition().x &&
 			sf::Mouse::getPosition(window).x <= TOP_BAR.getPosition().x + TOP_BAR.getSize().x &&
@@ -45,29 +56,38 @@ public:
 		{
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 			{
-				GUI_BACKGROUND.setPosition(sf::Mouse::getPosition(window).x + GUI_BACKGROUND.getSize().x / sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y + GUI_BACKGROUND.getSize().y / 2.0f - 10.0f);
+				GUI_BACKGROUND.setPosition(sf::Mouse::getPosition(window).x + GUI_BACKGROUND.getSize().x
+					/ sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y + GUI_BACKGROUND.getSize().y / 2.0f - 10.0f);
 				TOP_BAR.setPosition(sf::Vector2f(GUI_BACKGROUND.getGlobalBounds().left, GUI_BACKGROUND.getGlobalBounds().top));
 				WINDOW_TEXT.setPosition(sf::Vector2f(GUI_BACKGROUND.getGlobalBounds().left + 8.0f, GUI_BACKGROUND.getGlobalBounds().top + 2.0f));
 			}
 		}
-		
-	}
 
-	void Text(std::string TEXT, sf::Color TEXT_COLOR, sf::RenderWindow &window, int slot)
-	{		
-		text guiTEXT(GUI_BACKGROUND, slot, TEXT, TEXT_COLOR, window);		
 	}
 
 
-
+public:
+	//void Text(std::string TEXT, sf::Color TEXT_COLOR, sf::RenderWindow& window, int slot)
+	//{
+	//	text guiTEXT(GUI_BACKGROUND, slot, TEXT, TEXT_COLOR, window);
+	//}
 
 
 private:
+	
 	sf::RectangleShape GUI_BACKGROUND;
 	sf::RectangleShape TOP_BAR;
 	sf::Text WINDOW_TEXT;
 	sf::Font font;
+	int ELEMENT_COUNT = 0;
 	bool flag = true;
 	//bool slots[5] = {false, false, false, false, false};
+
+
+	struct GUI_IDS {
+		sf::RectangleShape GUI_PID;
+		sf::RectangleShape TOP_BAR;
+	};
+
 
 };
